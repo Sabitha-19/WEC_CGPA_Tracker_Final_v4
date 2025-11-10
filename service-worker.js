a012f7acbb085a3c4be2863ce518ccd94c48abf5
Sabitha-19 <sabithas7049@gmail.com>
2025-11-10 22:01:40 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "wec-cgpa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
